01fdb0dbc76c8d2bcc5762b4654848c7d3fd04ae
Gigalima <douglima@outlook.com>
2024-02-13 15:49:42 -0300

    Adicionando Estrutura do Projeto

---
+++ src/main/java/br/com/doug/workout/WorkoutApplication.java
@@ -0,0 +1,13 @@
+package br.com.doug.workout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutApplication.class, args);
+	}
+
+}
